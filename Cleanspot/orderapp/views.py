2cd80bec45b79ace9a116b3624d9f0c23a68a9f2
krivochenko_andrey@mail.ru <25jk8Dety>
2021-12-21 17:30:20 +0700

    Сделал форму в шаблоне добавления помещения

--- Cleanspot/orderapp/views.py
+++ Cleanspot/orderapp/views.py
@@ -8,4 +8,10 @@ class AddPremiseToOrderView(FormView):
     success_url = '/'
     template_name = 'orderapp/orderEntity-rooms.html'
 
-
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            print(form)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
